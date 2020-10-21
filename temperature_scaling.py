Fix scaling issue and bugs

temperature_scaling.py
@@ -57,10 +57,13 @@ class ModelWithTemperature(nn.Module):
         print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
 
         # Next: optimize the temperature w.r.t. NLL
-        optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
+        # optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
+        optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=10000)
 
         def eval():
-            loss = nll_criterion(self.temperature_scale(logits), labels)
+            # loss = nll_criterion(self.temperature_scale(logits), labels)
+            loss = ece_criterion(self.temperature_scale(logits), labels) #
+            optimizer.zero_grad() # 
             loss.backward()
             return loss
         optimizer.step(eval)